1a3b4f830ae9542802e44c928f6c9b9dc4e7eddf
TheoBrindejoncENI <49983279+TheoBrindejoncENI@users.noreply.github.com>
2021-04-30 13:17:33 +0200

    Add files via upload

---
+++ Carre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TP01
+{
+    public class Carre : Rectangle
+    {
+        public override int Largeur => Longueur;
+
+        public override string ToString()
+        {
+            return $"Carre de cote={Longueur}" + Environment.NewLine + base.ToString(true);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TP01")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TP01")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
